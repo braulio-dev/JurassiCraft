7b319a48e5dd8369b5a30bfe8180f992771e4da9
RenataSz4 <renatasan0411@gmail.com>
2024-03-05 11:33:17 -0800

    fix

--- src/main/resources/shader/fragment.fs
+++ src/main/resources/shader/fragment.fs
@@ -1,10 +1,10 @@
 #version 330
 
 in vec2 fragTexture;
-out vec4 texture;
+out vec4 tex;
 
 uniform sampler2D tex_sampler;
 
 void main() {
-    texture = texture(tex_sampler, fragTexture);
+    tex = texture(tex_sampler, fragTexture);
 }
